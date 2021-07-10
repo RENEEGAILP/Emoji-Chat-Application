10d6b8d8eec607bfbe4547f957b630439f7c4dc4
RENEEGAILP <22723224+RENEEGAILP@users.noreply.github.com>
2021-07-10 15:36:37 -0400

    Added a textview with the username for the user main page

--- NUMAD21SuVocab/app/src/main/java/edu/neu/madcourse/numad21su_vocab/UserPageActivity.java
+++ NUMAD21SuVocab/app/src/main/java/edu/neu/madcourse/numad21su_vocab/UserPageActivity.java
@@ -2,8 +2,10 @@ package edu.neu.madcourse.numad21su_vocab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class UserPageActivity extends AppCompatActivity {
 
@@ -18,6 +20,9 @@ public class UserPageActivity extends AppCompatActivity {
         {
             user_name = extras.getString("user_name");
         }
+        TextView welcome_textview = findViewById( R.id.welcome_textview );
+        welcome_textview.setText( "Welcome " +user_name +"!!");
+        welcome_textview.setVisibility( View.VISIBLE );
     }
 
     public void onClick(View view)
@@ -25,9 +30,14 @@ public class UserPageActivity extends AppCompatActivity {
         switch (view.getId())
         {
             case R.id.send_button:
-
+                Intent send_intent =new Intent(getApplicationContext(),SendActivity.class);
+                send_intent.putExtra( "user_name",user_name );
+                startActivity( send_intent );
                 break;
             case R.id.show_activity_button:
+                Intent display_all_intent =new Intent(getApplicationContext(),DisplayAllActivity.class);
+                display_all_intent.putExtra( "user_name",user_name );
+                startActivity( display_all_intent );
                 break;
         }
     }
