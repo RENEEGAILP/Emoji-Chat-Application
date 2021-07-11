4102194c064582693e641ee29ce2ae72261f29a7
RENEEGAILP <22723224+RENEEGAILP@users.noreply.github.com>
2021-07-11 10:56:10 -0400

    Added functionality to display sent message count of user. Updated by Gail, Kritika and Shaili

--- NUMAD21SuVocab/app/src/main/java/edu/neu/madcourse/numad21su_vocab/UserPageActivity.java
+++ NUMAD21SuVocab/app/src/main/java/edu/neu/madcourse/numad21su_vocab/UserPageActivity.java
@@ -1,11 +1,20 @@
 package edu.neu.madcourse.numad21su_vocab;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserPageActivity extends AppCompatActivity {
 
@@ -23,6 +32,8 @@ public class UserPageActivity extends AppCompatActivity {
         TextView welcome_textview = findViewById( R.id.welcome_textview );
         welcome_textview.setText( "Welcome " + userName +"!!");
         welcome_textview.setVisibility( View.VISIBLE );
+
+        displaySentMessageCount();
     }
 
     public void onClick(View view)
@@ -34,12 +45,29 @@ public class UserPageActivity extends AppCompatActivity {
                 send_intent.putExtra( "userName", userName );
                 startActivity( send_intent );
                 break;
-            case R.id.show_activity_button:
+            case R.id.show_received_activity_button:
                 Intent display_all_intent =new Intent(getApplicationContext(), DisplayReceivedActivity.class);
                 display_all_intent.putExtra( "userName", userName );
                 startActivity( display_all_intent );
                 break;
         }
+    }
+
+    public void displaySentMessageCount()
+    {
+        DatabaseReference db_ref = FirebaseDatabase.getInstance().getReference("ChatInfo");
+        Query query = db_ref.orderByChild( "senderName" ).equalTo( userName );
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                TextView sentMessageCount = findViewById( R.id.sent_message_count_textview );
+                sentMessageCount.setText( "You have sent a total of "+snapshot.getChildrenCount() +" mesages" );
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
 
